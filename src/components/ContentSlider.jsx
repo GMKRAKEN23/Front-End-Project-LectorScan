b1f13132fd87de67c84957f42d07bea7d95334ff
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-04-16 16:57:37 +0200

    modif problem esLint apostrophe

--- src/components/ContentSlider.jsx
+++ src/components/ContentSlider.jsx
@@ -1,17 +1,24 @@
 import slider1 from '/assets/img_slides/slide_1.jpeg';
 
-function ContentSlider(){
+function ContentSlider({name="One Piece", chapter="Chapitre 900",  date="30 juin 2025" }){
 
     return(
-        <div>
-            <div className="">
-
+        <div className='flex justify-around items-center h-full'>
+            <div className='w-4/12 mx-3 flex flex-col'>
+                <h2 className='text-6xl text-neutral-50 my-3'>{name}</h2>
+                <h3 className='text-neutral-50 my-3'>{chapter}</h3>
+                <p className='text-neutral-50 my-3'>{date}</p>
+                <p className='text-neutral-50 my-3'>Alors qu&apos;ils voguent à travers les mers infinies, 
+                    l&apos;équipage du Chapeau de Paille, mené par le charismatique Luffy, 
+                    poursuit son rêve ultime : trouver le légendaire trésor du One Piece. 
+                </p>
+                <p className='text-neutral-50 my-3'>Reading time: <span>10 min.</span></p>
+                <a href="#" className='text-neutral-50 my-3 uppercase'>read now</a>
             </div>
             <div>
-                <div className=''>
-                   <img src={slider1} alt="one_piece_content_one" className='hidden rounded lg:rounded-2xl'/> 
+                <div className='mx-3'>
+                   <img src={slider1} alt="one_piece_content_one" className='w-full h-[70vh] rounded lg:rounded-2xl brightness-110'/> 
                 </div>
-                
             </div>
         </div>
     )
