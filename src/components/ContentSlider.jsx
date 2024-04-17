cbe5ff4352283e0c5293cf6747f10064a3c22af9
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-04-17 09:42:16 +0200

    add feature button

--- src/components/ContentSlider.jsx
+++ src/components/ContentSlider.jsx
@@ -9,16 +9,16 @@ function ContentSlider({name="One Piece", chapter="",  date="", time="10", conte
                 <p className='text-neutral-50 my-3 tracking-widest text-lg'>{content}</p>
                 <p className='text-neutral-50 my-3 text-xl tracking-widest'>Reading time : <span className="bg-red-600 border-2 border-[#0a0a0a] text-lg px-1">{time} min</span></p>
                 <a href="#" className='text-neutral-50 my-3 uppercase text-2xl py-5 text-center transition duration-700 tracking-widest relative hover:bg-red-600 
-                  shadow_custom_red'>
+                  shadow_custom_red button'>
                 <div className="button_read_before"></div>
-                <div className="button_read_after"></div>
+                <div className="button_read_after "></div>
                 read now
                 </a>
 
             </div>
             <div>
                 <div className='mx-3'>
-                   <img src={slider} alt="one_piece_content_one" className='w-full h-[70vh] rounded rounded-2xl brightness-110 shadow-custom'/> 
+                   <img src={slider} alt="one_piece_content_one" className='w-full min-w-[668px] h-[70vh] rounded rounded-2xl brightness-110 shadow-custom'/> 
                 </div>
             </div>
         </div>
