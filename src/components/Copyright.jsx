2d107ba695394f02f1630fc0fad06a15919fc68a
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-04-17 13:24:52 +0200

    add component coyrigth

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,9 @@
+function Copyright() {
+    return (
+        <div className="relative text-neutral-50 text-center py-2.5">
+            <span className="text-base text-neutral-50 ">© 2024. All rights reserved by LectorScan.</span>
+        </div>
+    )
+}
+
+export default Copyright;
--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,10 +1,12 @@
 import ImageSlider from "../../src/components/ImageSlider";
+import Copyright from "../components/Copyright";
 
 function Content () {
 
     return (
         <div>
             <ImageSlider/>  
+            <Copyright />
         </div>  
     )
 }
