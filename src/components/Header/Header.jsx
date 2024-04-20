51b1a9da6d29cee07de4ec262051be6bf101fcf5
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-04-20 23:27:11 +0200

    modification size burgermenu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
                 <div className="flex lg:hidden">
                     <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                         <span className="sr-only">Open main menu</span>
-                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <svg className="h-9 w-9" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
                     </button>
--- src/components/ServiceDescription.jsx
+++ src/components/ServiceDescription.jsx
@@ -1,13 +1,13 @@
 function ServiceDescription() {
     return (
-        <div className="h-auto my-6 lg:h-96">
-            <div className="flex flex-col h-full place-content-evenly grid col-2 py-2">
+        <div className="h-auto lg:h-96">
+            <div className="flex flex-col h-full place-content-evenly grid col-2">
                 <div>
-                    <h2 className="text-neutral-50 text-center font-semibold w-3/4 lg:w-3/5 mx-auto py-4 tracking-widest text-lg md:text-xl lg:text-2xl">LectorScan, your online gateway to a vast library of manga.</h2>
+                    <h2 className="text-neutral-50 text-center font-semibold w-3/4 lg:w-3/5 mx-auto pb-4 tracking-widest text-lg md:text-xl lg:text-2xl">LectorScan, your online gateway to a vast library of manga.</h2>
                     <h3 className="text-neutral-50 text-center font-medium w-3/4 lg:w-2/5 mx-auto py-2 tracking-widest text-base md:text-base lg:text-lg">We are committed to providing top-quality services and unparalleled support.</h3>
                 </div>
 
-                <div className="grid grid-cols-2 gap-5 lg:flex lg:justfiy-between w-full-screen mx-auto lg:space-x-14 mx-2">
+                <div className="grid grid-cols-2 gap-5 lg:flex lg:justfiy-between w-full-screen mx-auto lg:space-x-14 mx-2 py-10">
                     <div className="flex flex-col items-center text-center">
                         <li className="list-none bg-neutral-50 border-2 border-red-600 rounded-full flex justify-center items-center w-16 h-16 my-3">
                             <i className='bx bxs-book-alt text-red-600 text-2xl'></i>
