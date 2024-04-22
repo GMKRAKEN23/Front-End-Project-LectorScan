f0ebbfcf757dc3236cfe62162117dd062ad85cb4
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-04-22 23:19:36 +0200

    add card popularManga

--- src/components/PopularManga.jsx
+++ src/components/PopularManga.jsx
@@ -1,21 +1,28 @@
 import ChapterBerserk from "../data/ChapterBerserk.json";
 import ChapterOnePiece from "../data/ChapterOnePiece.json";
+import ChapterChainsawMan from "../data/ChapterChainsawMan.json";
+import ChapterJujutsuKaizen from "../data/ChapterJujutsuKaizen.json"
 
 function PopularManga() {
-    const mangaData = [...ChapterBerserk.CollectionBerserk, ...ChapterOnePiece.CollectionOnePiece];
+    const mangaData = 
+    [...ChapterBerserk.CollectionBerserk, 
+    ...ChapterOnePiece.CollectionOnePiece,
+    ...ChapterChainsawMan.CollectionChainsawMan,
+    ...ChapterJujutsuKaizen.CollectionJujutsuKaizen,
+];
 
     return (
-        <div className="h-96">
-            <div className="flex flex-col h-96">
-                <div>
-                    <h2 className='text-neutral-50'>Popular Manga</h2>
+        <div className="my-14">
+            <div className="flex flex-col">
+                <div className="h-auto flex justify-center lg:justify-start lg:mx-3 ">
+                    <h2 className='text-neutral-50 tracking-widest font-semibold py-4 text-2xl lg:text-3xl'>Popular Manga</h2>
                 </div>
-                <div>
+                <div className="flex flex-col items-center lg:flex-row lg:justify-around cursor-pointer">
                     {mangaData.map((manga, index) => (
-                        <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg bg-neutral-50 m-2">
+                        <div key={index} className="max-w-sm w-72 h-96 rounded-lg overflow-hidden shadow-lg bg-neutral-50 m-2">
                             <img className="w-full" src={`/src/${manga.chapter[0].image}`} alt="Sunset in the mountains" />
                             <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                                <div className="font-bold text-xl mb-2"><h4>{manga.title}</h4></div>
                                 <p className="text-gray-700 text-base">
                                     {manga.chapter[0].pages}
                                 </p>
