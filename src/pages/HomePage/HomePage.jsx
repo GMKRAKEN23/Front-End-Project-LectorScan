07c6dbc0d107b3ac39ca77dbb0a062d35bc55458
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-04-24 17:11:52 +0200

    finished popular manga

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import ImageSlider from "../../components/ImageSlider";
 import Copyright from "../../components/Footer/Copyright";
 import Footer from "../../components/Footer/Footer";
@@ -6,20 +5,13 @@ import PromotionChapter from "../../components/PromotionChapter";
 import ServiceDescription from "../../components/ServiceDescription";
 import PopularManga from "../../components/PopularManga";
 
-import MangaData from "../../utils/joinsImport";
 
 function Content() {
-    const [mangaData, setMangaData] = useState([]);
-
-    useEffect(() => {
-        const combinedData = Object.values(MangaData).flatMap(collection => collection); 
-        setMangaData(combinedData);
-    }, []);
-
+ 
     return (
         <div>
             <ImageSlider />
-            <PopularManga mangaData={mangaData}/>
+            <PopularManga />
             <ServiceDescription />
             <PromotionChapter />
             <Footer />
--- src/utils/joinsImport.js
+++ src/utils/joinsImport.js
@@ -1,6 +1,8 @@
 import BerserkData from "../data/ChapterBerserk.json";
 import OnePieceData from "../data/ChapterOnePiece.json";
+import ChainsawManData from "../data/chapterChainsawMan";
+import JujutsuKaizenData from "../data/chapterJujutsuKaizen";
 
-const MangaData = { BerserkData, OnePieceData };
+const MangaData = { BerserkData, OnePieceData, ChainsawManData, JujutsuKaizenData };
 
 export default MangaData;
