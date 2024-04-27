5b0a728f1e42a8787c5fd9758d6bfe25181ac6ef
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2024-04-27 23:19:17 +0200

    modification responsive column

--- src/components/NewChapterManga.jsx
+++ src/components/NewChapterManga.jsx
@@ -22,17 +22,17 @@ function NewChapterManga(){
 
 
     return (
-        <div className="hidden sm:block">
+        <div className="hidden lg:block">
             <div><h2 className="text-neutral-50 text-center tracking-widest font-bold py-5 text-3xl">New chapter Manga</h2></div>
             {mangaData.map(( manga, index) => (
-                <div key={index} className="flex items-center card w-[21rem] h-32 sm:w-96 sm:h-32 border-2 rounded-xl my-5 px-4 bg-[#1d1e20] cursor-pointer  hover:scale-[102%] transform transition duration-300 
+                <div key={index} className="flex items-center h-32 w-96 sm:h-32 border-2 rounded-xl my-5 px-4 bg-[#1d1e20] cursor-pointer  hover:scale-[102%] transform transition duration-300 
                 ease-in-out shadow_custom_red-20">
-                    <div className=""><span className="sm:text-7xl text-6xl text-red-600">{numbers[index]}</span></div>
-                    <div className="flex jusitfy-center items-center">
-                        <div className="h-full mx-2">
-                            <img className="w-28 h-24 object-cover" src={`/src/${manga.chapter[0].image}`} alt="new_chapter_manga" />
+                    <div className="mx-1"><span className="sm:text-7xl text-6xl text-red-600">{numbers[index]}</span></div>
+                    <div className="flex justify-center items-center">
+                        <div className="h-full w-28 mx-3">
+                            <img className=" h-24 object-cover" src={`/src${manga.chapter[0].image}`} alt="new_chapter_manga" />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col mx-3">
                             <h3 className="text-neutral-50 tracking-widest text-base font-bold text-nowrap">{manga.title}</h3>
                             <h4 className="font-bold text-xs text-neutral-50 tracking-widest py-4">{manga.chapter[0].title}</h4>
                             <div className="flex py-1">
